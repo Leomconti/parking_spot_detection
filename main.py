b511d4b24863833632eb3f7fe9db386767e65c24
Leonardo Mosimann Conti <leomconti@gmail.com>
2023-06-15 13:30:26 -0300

    Added functions for each thing, and arguments for with some use cases done

--- main.py
+++ main.py
@@ -1,16 +1,55 @@
 import cv2
 import numpy as np
+import argparse
+import os
+import ast
 
 rectangles = []
-current_rectangle = []
+current_rectangle = None
 drawing = False
 image = None
 clone = None
 
-# Function to draw the rectangles in the image
+
+def get_rectangles_from_lot(lot_name):
+    path = f"/Users/leonardomosimannconti/computer_vision/parking_spot_detection/spots/{lot_name}.txt"
+
+    rectangles = []
+    try:
+        with open(path, "r") as f:
+            for line in f:
+                line = line.strip()
+                # line example = ((x, y), (x, y))
+                rectangle = ast.literal_eval(line)
+                rectangles.append(rectangle)
+
+        print("Get rectangles from lot")
+        print(rectangles)
+        return rectangles
+    except Exception as e:
+        print(e)
+        return []
+
+
+def save_rectangles_to_lot(lot_name, rectangles):
+    path = f"/Users/leonardomosimannconti/computer_vision/parking_spot_detection/spots/{lot_name}.txt"
+    with open(path, "a+") as f:
+        for rectangle in rectangles:
+            print(rectangle)
+            f.write(str(rectangle))
+            f.write("\n")
+
+    return rectangles
+
+
+def draw_rectangles(image, rectangles):
+    for rect in rectangles:
+        cv2.rectangle(image, rect[0], rect[1], (0, 255, 0), 2)
+    return image
+
+
 def click_and_crop(event, x, y, flags, param):
-    # instead of leaving as global maybe can create a class or something and return rectangle, or not, just use global
-    global rectangles, current_rectangle, drawing, image, clone
+    global drawing, image, clone, rectangles, current_rectangle
 
     # if the left mouse button is clicked, record the starting points and start drawing
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -28,43 +67,86 @@ def click_and_crop(event, x, y, flags, param):
     elif event == cv2.EVENT_LBUTTONUP:
         # record the ending (x, y) coordinates and indicate that the rectangle drawing has finished
         current_rectangle.append((x, y))
-        drawing = False # cropping is finished
+        drawing = False  # cropping is finished
 
-        # draw a rectangle around the region of interest
-        if abs(current_rectangle[0][0] - current_rectangle[1][0]) > 10 and abs(current_rectangle[0][1] - current_rectangle[1][1]) > 10:  # avoid very small boxes
-            cv2.rectangle(image, current_rectangle[0], current_rectangle[1], (0, 255, 0), 2)
-            clone = image.copy()
-            # Update the clone so when drawing the next rectangle, the previous ones are still there
-            # Show the image with the rectangle for no delay so the user can draw the next rectangle
-            cv2.imshow("image", image)
-            # Add the rectangle to the list of rectangles
+        cv2.rectangle(
+            image, current_rectangle[0], current_rectangle[1], (0, 255, 0), 2)
+        clone = image.copy()
+        # Update the clone so when drawing the next rectangle, the previous ones are still there
+        # Show the image with the rectangle for no delay so the user can draw the next rectangle
+        cv2.imshow("image", image)
+        # Add the rectangle to the list of rectangles
+        # Do not append very small rectangles
+        if abs(current_rectangle[0][0] - current_rectangle[1][0]) > 10 and abs(current_rectangle[0][1] - current_rectangle[1][1]) > 10:
             rectangles.append(tuple(current_rectangle))
 
 
-# load the image, clone it, and setup the mouse callback function
-image = cv2.imread("yolov8/2013-03-06_07_05_01.jpg")
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
+def main(args):
+    global image, clone, lot_name, rectangles
 
-while True:
-    # display the image and wait for a keypress to do an action or break
-    cv2.imshow("image", image)
-    key = cv2.waitKey(1) & 0xFF
+    filepath = args["filepath"]
+    webcam_index = args["webcam"]
+    drawing_mode = args["draw"]
+    lot_name = args["name"]
 
-    # # if the 'r' key is pressed, reset the cropping region
-    # if key == ord("r"):
-    #     image = clone.copy()
+    rectangles = get_rectangles_from_lot(lot_name)
 
-    # if the 'q' key is pressed, break from the loop
-    if key == ord("q"):
-        break
+    if webcam_index is not None:
+        video = cv2.VideoCapture(webcam_index)
+    elif filepath is not None:
+        video = cv2.VideoCapture(filepath)
+    else:
+        raise Exception(
+            "No video source provided, please enter webcam index or filepath")
 
-# # if there are two points, then crop the region of interest
-# # from the image and display it
-# for rect in rectangles:
-#     roi = clone[rect[0][1]:rect[1][1], rect[0][0]:rect[1][0]]
-#     cv2.imshow("ROI", roi)
-#     cv2.waitKey(0)
+    if drawing_mode == 1:
+        while True:
+            cv2.namedWindow("image")
+            cv2.setMouseCallback("image", click_and_crop)
+            ret, frame = video.read()
+            if not ret:
+                # If the video ends, reset the video capture to the beginning
+                video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                continue
 
-cv2.destroyAllWindows()
+            clone = frame.copy()
+            draw_rectangles(frame, rectangles)
+            cv2.imshow("image", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            if key == ord("q"):
+                break
+
+    else:
+        # start the detection and display in real time
+        pass
+
+    print(rectangles)
+    save_rectangles_to_lot(lot_name, rectangles)
+    video.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    # -f parking_spot_detection/pklot_video.mp4 -d 1 -n pk1
+
+    # ap = argparse.ArgumentParser()
+    # ap.add_argument("-f", "--filepath", type=str, default=None,
+    #                 help="Where's the recording at")
+    # ap.add_argument("-w", "--webcam", type=int, default=None,
+    #                 help="Index of the webcam on system")
+    # ap.add_argument("-d", "--draw", type=int, default=0,
+    #                 help="Draw the rectangles (0: No / 1: Yes)", required=True)
+    # ap.add_argument("-n", "--name", type=str, default="",
+    #                 help="Name of the parking lot", required=True)
+
+    # args = vars(ap.parse_args())
+
+    args = {
+        "filepath": "/Users/leonardomosimannconti/computer_vision/parking_spot_detection/pklot_video.mp4",
+        "draw": 1,
+        "name": "pk1",
+        "webcam": None
+    }
+
+    main(args)
